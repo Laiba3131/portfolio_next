1f73c0ae43901227628c4377c7ae2c9109172b08
Laiba Azam <129585341+Laiba3131@users.noreply.github.com>
2026-02-23 22:03:44 +0500

    skills

--- components/TechSlider.jsx
+++ components/TechSlider.jsx
@@ -6,62 +6,71 @@ import "swiper/css";
 import Image from "next/image";
 
 const techs = [
-    { name: "Flutter", src: "/images/react.png" },
-    { name: "Firebase", src: "/images/node.png" },
-    { name: "GetX", src: "/images/nextjs.png" },
-    { name: "BLoC", src: "/images/bootstrap.png" },
-    { name: "Provider", src: "/images/tailwindcss.png" },
-    { name: "GitHub", src: "/images/github.png" },
-    { name: "REST API", src: "/images/wordpress.png" },
-        { name: "Bootstrap", src: "/images/bootstrap.png" },
-    { name: "React", src: "/images/react.png" },
-    { name: "Next.js", src: "/images/nextjs.png" },
-    { name: "JavaScript", src: "/images/node.png" },
-    { name: "Tailwind", src: "/images/tailwindcss.png" },
-    { name: "WordPress", src: "/images/wordpress.png" },
+  { name: "Flutter", src: "/images/flutter.png" },
+  { name: "Firebase", src: "/images/firebase.png" },
+  //   { name: "GetX", src: "/images/nextjs.png" },
+  //   { name: "BLoC", src: "/images/bootstrap.png" },
+  //   { name: "Provider", src: "/images/tailwindcss.png" },
+  { name: "GitHub", src: "/images/github.png" },
+  //   { name: "REST API", src: "/images/wordpress.png" },
+  { name: "Bootstrap", src: "/images/bootstrap.png" },
+  { name: "React", src: "/images/react.png" },
+  { name: "Next.js", src: "/images/nextjs.png" },
+  { name: "JavaScript", src: "/images/node.png" },
+  { name: "Tailwind", src: "/images/tailwindcss.png" },
+  { name: "WordPress", src: "/images/wordpress.png" },
 ];
 
 export default function TechSlider() {
-    return (
-        <>
-            <div className="px-6 md:px-10 lg:px-20 bg-linear-to-r from-[#B18325]/50 via-[#0000]/55 to-[black]/60 py-12 border-y border-[#B18325]/10 backdrop-blur-sm">
-                <Swiper
-                    modules={[Autoplay]}
-                    loop={true}
-                    spaceBetween={30}
-                    speed={3000}
-                    slidesPerView={1}
-                    autoplay={{
-                        delay: 0,
-                        disableOnInteraction: false,
-                    }}
-                    breakpoints={{
-                        320: {
-                            slidesPerView: 2,
-                            spaceBetween: 20,
-                        },
-                        640: {
-                            slidesPerView: 3,
-                            spaceBetween: 30,
-                        },
-                        768: {
-                            slidesPerView: 4,
-                            spaceBetween: 40,
-                        },
-                        1024: {
-                            slidesPerView: 5,
-                            spaceBetween: 50,
-                        },
-                    }}
-                    className="flex items-center"
-                >
-                    {techs.map((tech, i) => (
-                        <SwiperSlide key={i} className="flex justify-center items-center opacity-60 hover:opacity-100 transition-opacity duration-300">
-                            <Image src={tech.src} alt={tech.name} width={130} height={130} className="" />
-                        </SwiperSlide>
-                    ))}
-                </Swiper>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="px-6 md:px-10 lg:px-20 bg-linear-to-r from-[#B18325]/50 via-[#0000]/55 to-[black]/60 py-12 border-y border-[#B18325]/10 backdrop-blur-sm">
+        <Swiper
+          modules={[Autoplay]}
+          loop={true}
+          spaceBetween={30}
+          speed={3000}
+          slidesPerView={1}
+          autoplay={{
+            delay: 0,
+            disableOnInteraction: false,
+          }}
+          breakpoints={{
+            320: {
+              slidesPerView: 2,
+              spaceBetween: 20,
+            },
+            640: {
+              slidesPerView: 3,
+              spaceBetween: 30,
+            },
+            768: {
+              slidesPerView: 4,
+              spaceBetween: 40,
+            },
+            1024: {
+              slidesPerView: 5,
+              spaceBetween: 50,
+            },
+          }}
+          className="flex items-center"
+        >
+          {techs.map((tech, i) => (
+            <SwiperSlide
+              key={i}
+              className="flex justify-center items-center opacity-60 hover:opacity-100 transition-opacity duration-300"
+            >
+              <Image
+                src={tech.src}
+                alt={tech.name}
+                width={130}
+                height={130}
+                className=""
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </>
+  );
 }
